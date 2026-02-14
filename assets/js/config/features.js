aaf50c26ce38722799c9b003dd8237e916e867c1
anyjiujitsu <anynejiujitsu@gmail.com>
2026-02-14 10:29:33 -0500

    Delete assets/js/config directory

--- assets/js/config/features.js
+++
@@ -1,7 +0,0 @@
-// assets/js/config/features.js
-// purpose: single-source feature flags for optional UI/modules
-
-export const FEATURES = {
-  // Floating + button (FAB) that opens the "Add Entry" modal shell
-  FAB_ENTRY: true,
-};
