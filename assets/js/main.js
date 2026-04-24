da779586787097ffb892528cae74a2f2d7bfa4eb
anyjiujitsu <anynejiujitsu@gmail.com>
2026-04-24 14:23:52 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 // main.js
 // purpose: app bootstrap + data loading + render orchestration
 
-import { applyCustomization } from "../../customization.js";
 import { loadCSV, normalizeDirectoryRow, normalizeEventRow } from "./data.js?v=20260210-911";
 import { state, setIndexQuery, setEventsQuery, setIndexEventsQuery, setIndexDistanceMiles, setIndexDistanceFrom } from "./state.js?v=20260212-902";
 import { filterEvents } from "./filters.js?v=20260210-911";
@@ -66,6 +65,7 @@ function render(){
 }
 
 async function init(){
+  const { applyCustomization } = await import(`../../customization.js?v=${Date.now()}`);
   applyCustomization();
 
   wireViewToggle({ $, onIndexViewOpen: syncIndexDistanceUI });
