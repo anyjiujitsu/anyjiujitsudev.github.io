b9dbe728371355bf7fde589bae411466e88ab337
anyjiujitsu <anynejiujitsu@gmail.com>
2026-04-27 08:00:04 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ import { renderEventsGroups, renderIndexEventsGroups } from "./render.js?v=20260
 import { $ } from "./utils/dom.js?v=20260210-911";
 import { applyDistanceFilter } from "./utils/geo.js?v=20260212-902";
 import { initEventsPills, initIndexPills } from "./ui/pills.js?v=20260210-911";
-import { wireSearch, wireSearchSuggestions } from "./ui/search.js?v=20260427-eventszip-directapply";
+import { wireSearch, wireSearchSuggestions } from "./ui/search.js?v=20260427-events-zip-shared-002";
 import { closePricingPopup, wirePricingPopup } from "./ui/pricing.js";
 import { activeEventsState, setActiveEventsQuery, setViewUI, wireViewToggle } from "./ui/viewToggle.js";
 import { dirToIndexEventRow, ensureDistanceOriginOptions, filterIndexDirectoryAsEvents, syncDistanceUIFromState } from "./indexDirectory.js";
